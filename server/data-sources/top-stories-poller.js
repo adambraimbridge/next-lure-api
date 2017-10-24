8d2d5ea80418422ae05c308cd2ac636f130a4ee2
Rhys Evans <rhys.evans@ft.com>
2017-10-24 15:39:32 +0100

    tweaks  🐿 v2.5.16

--- server/data-sources/top-stories-poller.js
+++ server/data-sources/top-stories-poller.js
@@ -4,12 +4,10 @@ const { TEASER_PROPS } = require('../constants');
 
 const pollList = uuid => {
 	return new Poller({
-		url: `https://prod-coco-up-read.ft.com/lists/${uuid}`,
-		// url: `https://api.ft.com/lists/${uuid}`,
+		url: `https://api.ft.com/lists/${uuid}`,
 		options: {
 			headers: {
-				Authorization: ''
-				// 'x-api-key': process.env.FT_DEVELOPER_API_KEY
+				'X-Api-Key': process.env.API_KEY
 			},
 			timeout: 3000,
 			retry: false
@@ -28,14 +26,14 @@ const pollList = uuid => {
 
 const pollers = {
 	uk: pollList('520ddb76-e43d-11e4-9e89-00144feab7de'),
-	intl: pollList('b0d8e4fe-10ff-11e5-8413-00144feabdc0')
+	international: pollList('b0d8e4fe-10ff-11e5-8413-00144feabdc0')
 }
 
 
 module.exports = {
 	init: () => {
 		pollers.uk.start({ initialRequest: true });
-		pollers.intl.start({ initialRequest: true });
+		pollers.international.start({ initialRequest: true });
 	},
 	get: region => pollers[region].getData()
 }
