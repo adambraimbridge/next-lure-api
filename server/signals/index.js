b57ecb4b5ec0fc4eba1106c38f6def8c6fb52416
fenglish <asuka.ochi@ft.com>
2017-11-30 15:09:10 +0000

    add essential stories in signals  🐿 v2.5.16

--- server/signals/index.js
+++ server/signals/index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	relatedContent: require('./related-content'),
 	topStories: require('./top-stories'),
-	timeRelevantRecommendations: require('./time-relevant-recommendations')
+	timeRelevantRecommendations: require('./time-relevant-recommendations'),
+	essentialStories: require('./essential-stories')
 };
