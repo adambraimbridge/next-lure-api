36bfdd301533de886eab9f10ef3ee49116cd9de5
Luke Kavanagh <kavanagh@users.noreply.github.com>
2018-09-19 09:20:53 +0100

    Use display tag if present (#47)

--- server/lib/get-most-related-concepts.js
+++ server/lib/get-most-related-concepts.js
@@ -2,7 +2,11 @@ module.exports = content => {
 	if (!content.annotations) {
 		return undefined;
 	}
+
+	const displayTag = content.displayConcept && content.displayConcept.isDisplayTag ? content.displayConcept : null;
+
 	const mostRelatedConcepts = [
+		displayTag,
 		content.annotations.find(annotation => annotation.predicate === 'http://www.ft.com/ontology/annotation/about'),
 		content.annotations.find(annotation => annotation.predicate === 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy')
 	]
